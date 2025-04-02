10946b723b3d609fa17e586c949fbd57b57c8ba0
fearaizor <noluckanytime@gmail.com>
2025-04-02 14:06:41 +0700

    Add initial Spring Boot application structure with main class and tests

---
+++ src/test/java/com/ptithcm/sqa/SqaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ptithcm.sqa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
